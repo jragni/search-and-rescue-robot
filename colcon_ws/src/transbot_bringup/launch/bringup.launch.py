a1b040d1b286111ef2f0d94c6cb912b61f84c817
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-04-04 16:33:49 -0700

    add map fo link to odom

--- colcon_ws/src/transbot_bringup/launch/bringup.launch.py
+++ colcon_ws/src/transbot_bringup/launch/bringup.launch.py
@@ -123,6 +123,23 @@ def generate_launch_description():
         parameters=[{'linear_scale': 1.2}]
     )
 
+    # map to odom publsiher
+    ## TODO shift to transbot_nav, okay for dev for now
+    map_static_transform_publisher_node = Node(
+        package='tf2_ros',
+        executable='static_transform_publisher',
+        name='map_link_to_odom_link',
+        arguments=[
+            '--x', '0.0',
+            '--y', '0.0',
+            '--z', '0.00',
+            '--qx', '0',
+            '--qy', '0',
+            '--qw', '1',
+            '--frame-id', 'map',
+            '--child-frame-id', 'odom',
+      ]
+    )
 
     return LaunchDescription([
         declare_use_sim_time,
@@ -134,4 +151,5 @@ def generate_launch_description():
         lidar_launch,
         rgbd_camera_static_transform_publisher_node,
         astra_launch,
+        map_static_transform_publisher_node,
     ])
