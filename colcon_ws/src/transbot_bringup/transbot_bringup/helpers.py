fix rotation matrix

colcon_ws/src/transbot_bringup/transbot_bringup/helpers.py
@@ -38,8 +38,8 @@ def correct_gyro(ax, ay, az):
     """
     p = np.transpose(np.array([ax, ay, az]))
     rotation_matrix = [
-        [cos(-pi / 2), sin(-pi/2), 0],
-        [sin(-pi / 2), cos(-pi / 2), 0],
+        [cos(-pi / 2), sin(-pi / 2), 0],
+        [-sin(-pi / 2), cos(-pi / 2), 0],
         [0, 0, 1]
     ]
     corrected = np.dot(rotation_matrix, p)