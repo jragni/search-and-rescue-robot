2d69dbf9600fc6f2ddf4828973d269aa058b5abf
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-04-10 01:27:49 -0700

    create slam launch and remove launch from bringup.launch

--- colcon_ws/src/transbot_bringup/launch/bringup.launch.py
+++ colcon_ws/src/transbot_bringup/launch/bringup.launch.py
@@ -140,25 +140,7 @@ def generate_launch_description():
         remappings=[("odometry/filtered", "odom")],
     )
 
-    # rtab map test
-    parameters={
-        'frame_id':'base_footprint',
-        'map_frame_id': 'map',
-        'odom_frame_id': 'odom',
-        'use_sim_time':False,
-        'subscribe_rgbd':True,
-        'subscribe_scan':True,
-        'use_action_for_goal':True,
-        'qos_scan': 2,
-        'qos_image': 2,
-        'qos_imu': 2,
-        'Reg/Strategy':'1',
-        'Reg/Force3DoF':'true',
-        'RGBD/NeighborLinkRefining':'True',
-        'Grid/RangeMin':'0.2',
-        'Optimizer/GravitySigma':'0'
-    }
-
+    # TODO add image_transport nodes for rgb and depth
     remappings=[
         ('rgb/image', '/camera/color/image_raw'),
         ('rgb/camera_info', '/camera/color/camera_info'),
@@ -166,17 +148,6 @@ def generate_launch_description():
         ('odom', '/odom')
     ]
 
-    rtabmap_sync_node = Node(
-        package='rtabmap_sync', executable='rgbd_sync', output='screen',
-        parameters=[{'approx_sync':True, 'approx_sync_max_interval':0.01, 'use_sim_time':use_sim_time, 'qos':2 }],
-        remappings=remappings
-    )
-
-    rtab_slam_node = Node(
-        package='rtabmap_slam', executable='rtabmap', output='screen',
-        parameters=[parameters],
-        remappings=remappings,
-    )
 
     return LaunchDescription([
         declare_use_sim_time,
@@ -189,6 +160,4 @@ def generate_launch_description():
         lidar_launch,
         rgbd_camera_static_transform_publisher_node,
         astra_launch,
-        rtabmap_sync_node,
-        rtab_slam_node,
     ])
---
+++ colcon_ws/src/transbot_bringup/launch/slam.launch.py
@@ -0,0 +1,52 @@
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    rtab_parameters={
+        'frame_id':'base_footprint',
+        'map_frame_id': 'map',
+        'odom_frame_id': 'odom',
+        'use_sim_time':False,
+        'subscribe_rgbd':True,
+        'subscribe_scan':True,
+        'use_action_for_goal':True,
+        'qos_scan': 2,
+        'qos_image': 2,
+        'qos_imu': 2,
+        'Reg/Strategy':'1',
+        'Reg/Force3DoF':'true',
+        'RGBD/NeighborLinkRefining':'True',
+        'Grid/RangeMin':'0.2',
+        'Optimizer/GravitySigma':'0'
+    }
+
+    remappings=[
+        ('rgb/image', '/camera/color/image_raw'),
+        ('rgb/camera_info', '/camera/color/camera_info'),
+        ('depth/image', '/camera/depth/image_raw'),
+        ('odom', '/odom')
+    ]
+
+    rtabmap_sync_node = Node(
+        package='rtabmap_sync', executable='rgbd_sync', output='screen',
+        parameters=[{'approx_sync':True, 'approx_sync_max_interval':0.01, 'use_sim_time':false, 'qos':2 }],
+        remappings=remappings
+    )
+
+    rtab_slam_node = Node(
+        package='rtabmap_slam', executable='rtabmap', output='screen',
+        parameters=[rtab_parameters],
+        remappings=remappings,
+    )
+
+    return LaunchDescription([
+        rtabmap_sync_node,
+        rtab_slam_node,
+    ])
