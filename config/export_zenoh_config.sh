f9344ba37c8c08b90bc9be8f651207d8a69ebf2d
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-04-09 21:33:30 -0700

    fix bug with missing packages, update zenoh config

--- config/export_zenoh_config.sh
+++ config/export_zenoh_config.sh
@@ -4,3 +4,4 @@ export ZENOH_SESSION_CONFIG_URI=$HOME/search-and-rescue-robot/config/sessionconf
 export ZENOH_ROUTER_CONFIG_URI=$HOME/search-and-rescue-robot/config/routerconfig.json5
 export ZENOH_ROUTER_CHECK_ATTEMPT=-1
 export ZENOH_CONFIG_OVERRIDE="scouting/multicast/enabled=true"
+export ROS_DOMAIN_ID=30
