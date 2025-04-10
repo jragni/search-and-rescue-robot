1c84d20548ce23b923405a5971c0ac77473b6f70
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-04-09 20:18:55 -0700

    fix config :

--- config/export_zenoh_config.sh
+++ config/export_zenoh_config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE_PATH="/search-and-rescue-robot/config"
 export ZENOH_SESSION_CONFIG_URI=$HOME/search-and-rescue-robot/config/sessionconfig.json5
-export ZENOH_CLIENT_CONFIG_URI=$HOME/search-and-rescue-robot/config/clientconfig.json5
+export ZENOH_ROUTER_CONFIG_URI=$HOME/search-and-rescue-robot/config/routerconfig.json5
 export ZENOH_ROUTER_CHECK_ATTEMPT=-1
+export ZENOH_CONFIG_OVERRIDE="scouting/multicast/enabled=true"
