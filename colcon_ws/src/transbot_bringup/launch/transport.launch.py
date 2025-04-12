060e35d6ec683b059ad489213aa833734e436533
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-04-11 21:53:49 -0700

    add image transport test node

--- colcon_ws/src/transbot_bringup/launch/transport.launch.py
+++ colcon_ws/src/transbot_bringup/launch/transport.launch.py
@@ -12,15 +12,19 @@ from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
 
-    image_transport_remappings=[
+    rgb_image_transport_remappings=[
         ('in', '/camera/color/image_raw'),
-        ('out', '/camera/color/image_raw')
+        ('out/compressed', '/camera/color/image_raw')
     ]
 
-    image_transport_node = Node(
+    rgb_image_transport_node = Node(
         package="image_transport",
         executable="republish",
         output="screen",
         arguments=["raw", "compressed"],
         remapping=image_transport_remappings
     )
+
+    return LaunchDescription([
+        rgb_image_transport_node,
+    ])
