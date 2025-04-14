1ceb3a5fe08bd793b30be9e671748f925704aa90
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-04-13 23:20:17 -0700

    update imports

--- colcon_ws/src/transbot_bringup/launch/bringup.launch.py
+++ colcon_ws/src/transbot_bringup/launch/bringup.launch.py
@@ -4,7 +4,8 @@ import xacro
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration
-from launch.launch_description_sources import PythonLaunchDescriptionSource, XMLLaunchDescriptionSource
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from launch_ros.actions import Node
 
 from ament_index_python.packages import get_package_share_directory
