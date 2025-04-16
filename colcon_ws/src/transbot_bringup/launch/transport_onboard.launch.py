2d7ec8eac09cea3df7940e9b60c7472c97dc0b4e
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-04-15 18:54:37 -0700

    revert depth

--- colcon_ws/src/transbot_bringup/launch/transport_onboard.launch.py
+++ colcon_ws/src/transbot_bringup/launch/transport_onboard.launch.py
@@ -21,19 +21,6 @@ def generate_launch_description():
         arguments=['raw', 'compressed']
     )
 
-    depth_image_transport_node = Node(
-        package='image_transport',
-        executable='republish',
-        output='screen',
-        name='republish_onboard_depth',
-        remappings=[
-            ('in', '/camera/depth/image_raw'),
-            ('out/compressed', '/camera/depth/image_raw/compressed')
-        ],
-        arguments=['raw', 'compressed']
-    )
-
     return LaunchDescription([
         rgb_image_transport_node,
-        depth_image_transport_node,
     ])
