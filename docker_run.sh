ab830d55aaa2328f85001888ae2251833d3fe445
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-04-24 03:03:18 -0700

    add name

--- docker_run.sh
+++ docker_run.sh
@@ -1,1 +1,1 @@
-docker run -it --rm --user ubuntu --ipc=host --network=host -v /tmp/.X11-unix:/tmp/.X11-unix:rw --env=DISPLAY -v $PWD:/home/ubuntu/search-and-rescue-robot -v /dev/:/dev --device-cgroup-rule='c *:* rmw' ros2
+docker run -it --rm --user ubuntu --name ros2 --ipc=host --network=host -v /tmp/.X11-unix:/tmp/.X11-unix:rw --env=DISPLAY -v $PWD:/home/ubuntu/search-and-rescue-robot -v /dev/:/dev --device-cgroup-rule='c *:* rmw' ros_2_image
