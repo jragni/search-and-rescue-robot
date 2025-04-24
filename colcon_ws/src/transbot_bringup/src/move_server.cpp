a35c654d1830af8d07105522872fb9160f7f8d25
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-04-24 12:05:17 -0700

    update rplidar orientation, expand moveforward to move

--- colcon_ws/src/transbot_bringup/src/move_server.cpp
+++ colcon_ws/src/transbot_bringup/src/move_server.cpp
@@ -1,18 +1,18 @@
 #include "rclcpp/rclcpp.hpp"
 
 #include "geometry_msgs/msg/twist.hpp"
-#include "transbot_msgs/srv/move_forward.hpp"
+#include "transbot_msgs/srv/move.hpp"
 
 using namespace std::chrono_literals;
 
-class MoveForwardServer : public rclcpp::Node {
+class MoveServer : public rclcpp::Node {
   public:
-    MoveForwardServer() : Node("move_forward_service_server_node") {
+    MoveServer() : Node("move_service_server_node") {
       pub_ = this->create_publisher<geometry_msgs::msg::Twist>("/cmd_vel", 10);
-      srv_ = this->create_service<transbot_msgs::srv::MoveForward>(
-         "move_forward",
+      srv_ = this->create_service<transbot_msgs::srv::Move>(
+         "move",
          std::bind(
-          &MoveForwardServer::move_forward_callback,
+          &MoveServer::move_forward_callback,
           this,
           std::placeholders::_1,
           std::placeholders::_2
@@ -22,14 +22,19 @@ class MoveForwardServer : public rclcpp::Node {
 
   private:
     rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub_;
-    rclcpp::Service<transbot_msgs::srv::MoveForward>::SharedPtr srv_;
+    rclcpp::Service<transbot_msgs::srv::Move>::SharedPtr srv_;
 
     void move_forward_callback(
-      const std::shared_ptr<transbot_msgs::srv::MoveForward::Request> request,
-      const std::shared_ptr<transbot_msgs::srv::MoveForward::Response> response
+      const std::shared_ptr<transbot_msgs::srv::Move::Request> request,
+      const std::shared_ptr<transbot_msgs::srv::Move::Response> response
     ) {
       auto msg = geometry_msgs::msg::Twist();
-      msg.linear.x = request->velocity;
+      if (request->direction == "angular")  {
+        msg.angular.z = request->velocity;
+      } else {
+        msg.linear.x = request->velocity;
+      }
+
       for (int i = 0; i < request->time; i++) {
         pub_->publish(msg);
         std::this_thread::sleep_for(1000ms);
@@ -46,7 +51,7 @@ class MoveForwardServer : public rclcpp::Node {
 int main (int argc, char** argv) {
   rclcpp::init(argc, argv);
 
-  std::shared_ptr<MoveForwardServer> client = std::make_shared<MoveForwardServer>();
+  std::shared_ptr<MoveServer> client = std::make_shared<MoveServer>();
   rclcpp::spin(client);
 
   rclcpp::shutdown();
