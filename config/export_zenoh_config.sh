4f73e7e59ef9c24a7a0c38cdc3abbbee7c75ee87
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-04-24 12:04:28 -0700

    update zenoh config

--- config/export_zenoh_config.sh
+++ config/export_zenoh_config.sh
@@ -2,6 +2,6 @@
 
 export ZENOH_SESSION_CONFIG_URI=$HOME/search-and-rescue-robot/config/sessionconfig.json5
 export ZENOH_ROUTER_CONFIG_URI=$HOME/search-and-rescue-robot/config/routerconfig.json5
-export ZENOH_ROUTER_CHECK_ATTEMPT=-1
+export ZENOH_ROUTER_CHECK_ATTEMPTS=-1
 export ZENOH_CONFIG_OVERRIDE="scouting/multicast/enabled=true"
 export ROS_DOMAIN_ID=30
