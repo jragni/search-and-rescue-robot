75adad41385f62ad44561bbab2eb5e843a1eda50
Jhensen Ray Agni <jhensenrayagni@gmail.com>
2025-04-24 02:57:23 -0700

    format

--- docker_run.sh
+++ docker_run.sh
@@ -1,5 +1,4 @@
-docker run -it --rm --user ubuntu --ipc=host --uts=host --network=host \
--v /tmp/.X11-unix:/tmp/.X11-unix:rw --env=DISPLAY \ 
--v $PWD:/home/ubuntu/search-and-rescue-robot \
--v /dev/:/dev \
+docker run -it --rm --user ubuntu --ipc=host --uts=host \
+--network=host -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+--env=DISPLAY \ -v $PWD:/home/ubuntu/search-and-rescue-robot \ -v /dev/:/dev \
 --device-cgroup-rule='c *:* rmw' ros_2_image
